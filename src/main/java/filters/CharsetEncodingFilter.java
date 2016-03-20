e15f2591eb5b0d8b6126340ac3985cf786ff6224
savin <savin@Alex-PC>
2016-03-20 23:15:55 +0200

    Added UTF-8 encoding by Servlet Filter

---
+++ src/main/java/filters/CharsetEncodingFilter.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package filters;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+/**
+ *
+ * @author savin
+ */
+public class CharsetEncodingFilter implements Filter {
+
+    private String encoding = "utf-8";
+
+    @Override
+    public void destroy() {
+        
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        String encodingParam = filterConfig.getInitParameter("encoding");
+        if (encodingParam != null) {
+            encoding = encodingParam;
+        }
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        request.setCharacterEncoding(encoding);
+        chain.doFilter(request, response);
+    }
+
+}
