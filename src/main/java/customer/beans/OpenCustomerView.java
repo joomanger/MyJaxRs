b4cc17af0bcf078477fb782c792c76276b262ee3
savin <savin@SAVIN-W10.isd.local>
2016-09-20 11:34:38 +0300

    Работа над кастомерами

--- src/main/java/customer/beans/OpenCustomerView.java
+++ src/main/java/customer/beans/OpenCustomerView.java
@@ -1,6 +1,7 @@
 package customer.beans;
 
 import customer.entities.Address;
+import customer.entities.Country;
 import customer.entities.Customer;
 import customer.entities.RWAddress;
 import java.util.ArrayList;
@@ -29,20 +30,30 @@ public class OpenCustomerView extends AbstractView<Customer> {
     private FindCustomerSession fls;
     @Inject
     private SessionActions sa;
-    
-    private Customer openedEntity=new Customer();
-    
+
+    private Customer openedEntity = new Customer();
+
     private List<Address> selectedAddresses = new ArrayList<>();
     private List<RWAddress> selectedRWAddresses = new ArrayList<>();
     private List<RWAddress> filteredRWAddresses;
     private List<Address> filteredAddresses;
 
-    //Поля для создания новой строки
+    //Поля для создания новой строки ЖД адреса
     private RWStation station;
     private String rwBranch;
     private String rwRcvCode;
-    
+    //Поля для создания новой строки юр. адреса
+    private Country country;
     private String region;
+    private String region2;
+    private String postCode;
+    private String city;
+    private String fullAddress;
+    private Boolean ship_to;
+    private Boolean bill_to;
+    private Boolean vendor;
+    private Boolean activeStatus;
+    private Long duferco_site_use_id;
 
     @SuppressWarnings("PMD.UnusedPrivateMethod")
     @PostConstruct
@@ -133,7 +144,98 @@ public class OpenCustomerView extends AbstractView<Customer> {
     public void setRegion(String region) {
         this.region = region;
     }
-    
-    
+
+    public Country getCountry() {
+        return country;
+    }
+
+    public void setCountry(Country country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getFullAddress() {
+        return fullAddress;
+    }
+
+    public void setFullAddress(String fullAddress) {
+        this.fullAddress = fullAddress;
+    }
+
+    public Boolean getShip_to() {
+        return ship_to;
+    }
+
+    public void setShip_to(Boolean ship_to) {
+        this.ship_to = ship_to;
+    }
+
+    public Boolean getBill_to() {
+        return bill_to;
+    }
+
+    public void setBill_to(Boolean bill_to) {
+        this.bill_to = bill_to;
+    }
+
+    public Boolean getVendor() {
+        return vendor;
+    }
+
+    public void setVendor(Boolean vendor) {
+        this.vendor = vendor;
+    }
+
+    public Boolean getActiveStatus() {
+        return activeStatus;
+    }
+
+    public void setActiveStatus(Boolean activeStatus) {
+        this.activeStatus = activeStatus;
+    }
+
+    public Long getDuferco_site_use_id() {
+        return duferco_site_use_id;
+    }
+
+    public void setDuferco_site_use_id(Long duferco_site_use_id) {
+        this.duferco_site_use_id = duferco_site_use_id;
+    }
+
+    public String getPostCode() {
+        return postCode;
+    }
+
+    public void setPostCode(String postCode) {
+        this.postCode = postCode;
+    }
+
+    public String getRegion2() {
+        return region2;
+    }
+
+    public void setRegion2(String region2) {
+        this.region2 = region2;
+    }
+
+    public void clearAddressFields() {
+        region = null;
+        region2 = null;
+        postCode = null;
+        city = null;
+        fullAddress = null;
+        ship_to = false;
+        bill_to = false;
+        vendor = false;
+        activeStatus = false;
+        duferco_site_use_id = null;
+    }
 
 }
