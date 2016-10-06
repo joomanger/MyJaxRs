edb357d340828684e5fd6bcbcfdb91e9a015e31f
savin <savin@SAVIN-W10.isd.local>
2016-10-06 11:39:46 +0300

    Внесены небольшие изменения в PaymentTerm. Залит справочник PaymentTerm

--- src/main/resources/META-INF/load.sql
+++ src/main/resources/META-INF/load.sql
@@ -204,9 +204,9 @@ insert into lookupitemtl(lookupitemtl_id,meaning,description,language,lookupitem
 --Contract Purposes
 insert into lookup(lookup_id, activestatus, description, name,systemlookup) values(nextval('lookup_sq'),true,'Цели контракта','CONTRACT PURPOSES',true);
 insert into lookupitem(lookupitem_id,activestatus,valuez,lookup_id) values(nextval('lookupitem_sq'),true,'PURCHASE',currval('lookup_sq'));
-insert into lookupitemtl(lookupitemtl_id,meaning,description,language,lookupitem_id) values(nextval('lookupitemtl_sq'),'Покупака','Покупака','RU',currval('lookupitem_sq'));
+insert into lookupitemtl(lookupitemtl_id,meaning,description,language,lookupitem_id) values(nextval('lookupitemtl_sq'),'Покупка','Покупка','RU',currval('lookupitem_sq'));
 insert into lookupitemtl(lookupitemtl_id,meaning,description,language,lookupitem_id) values(nextval('lookupitemtl_sq'),'Purchase','Purchase','US',currval('lookupitem_sq'));
-insert into lookupitemtl(lookupitemtl_id,meaning,description,language,lookupitem_id) values(nextval('lookupitemtl_sq'),'Покупака','Покупака','UA',currval('lookupitem_sq'));
+insert into lookupitemtl(lookupitemtl_id,meaning,description,language,lookupitem_id) values(nextval('lookupitemtl_sq'),'Покупка','Покупка','UA',currval('lookupitem_sq'));
 
 insert into lookupitem(lookupitem_id,activestatus,valuez,lookup_id) values(nextval('lookupitem_sq'),true,'SALE',currval('lookup_sq'));
 insert into lookupitemtl(lookupitemtl_id,meaning,description,language,lookupitem_id) values(nextval('lookupitemtl_sq'),'Продажа','Продажа','RU',currval('lookupitem_sq'));
